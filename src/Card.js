6e59ccea6fe241616408397ac53487df946b206e
karim mokhtar <karimmokhtar28@gmail.com>
2020-07-16 12:18:01 +0200

    adding card component with static data

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card = (props) => {
+  const { robot } = props;
+  return (
+    <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+      <img
+        src={`https://robohash.org/${robot.id}?size=200x200`}
+        alt={robot.name}
+      />
+      <div>
+        <h2>{robot.name}</h2>
+        <p>{robot.email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "tachyons";
+
+import Card from "./Card";
+import {robots} from "./robots";
+
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {robots.map((robot) => (
+      <Card robot={robot} key={robot.id}/>
+    ))}
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
