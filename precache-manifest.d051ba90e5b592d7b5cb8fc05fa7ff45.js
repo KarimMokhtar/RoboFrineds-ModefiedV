78b8a19d592ea61d085c096fa5b840d35b9cbf65
karim mokhtar <karimmokhtar28@gmail.com>
2020-07-21 13:06:58 +0200

    Updates

---
+++ precache-manifest.d051ba90e5b592d7b5cb8fc05fa7ff45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e810708a91db1b9f2cca0e7c727f0d9e",
+    "url": "/RoboFrineds-ModefiedV/index.html"
+  },
+  {
+    "revision": "7341a4fc77d4a63b6dca",
+    "url": "/RoboFrineds-ModefiedV/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "43ae76ce2b467ed42a7c",
+    "url": "/RoboFrineds-ModefiedV/static/css/main.05510d95.chunk.css"
+  },
+  {
+    "revision": "7341a4fc77d4a63b6dca",
+    "url": "/RoboFrineds-ModefiedV/static/js/2.966b1d21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RoboFrineds-ModefiedV/static/js/2.966b1d21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43ae76ce2b467ed42a7c",
+    "url": "/RoboFrineds-ModefiedV/static/js/main.11c07c53.chunk.js"
+  },
+  {
+    "revision": "11b37834906db6f4cb2e",
+    "url": "/RoboFrineds-ModefiedV/static/js/runtime-main.5eaff0b5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFrineds-ModefiedV/static/media/SEGA.12a3db8b.woff"
+  }
+]);
